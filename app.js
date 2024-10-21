0d06a1c3bf615f7469e8c6afd182494fb51d77e0
SENA <SENA@ARMSCCCCTGOD158.FormacionSena.RED>
2024-10-21 17:13:05 -0500

    Actualizacion faltan las canciones

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
-const audio = document.getElementById("audio")
-const play = document.getElementById("play")
-const pause = document.getElementById("pause")
-const forward = document.getElementById("forward")
-const rewind = document.getElementById("rewind")
-const stop = document.getElementById("stop")
+import { playList } from "./js/app2.js";
 
+const audio = document.getElementById('audio')
+const play = document.getElementById('play')
+const pause = document.getElementById('pause')
+const forward = document.getElementById('forward')
+const rewind = document.getElementById('rewind')
+const stops = document.getElementById('stop')
+const playimg = document.getElementById('playimg')
+const nameArtist = document.getElementById('nameArtist')
+const nameSong = document.getElementById('nameSong')
+const audioSource = document.getElementById('audioSource')
+const nextSong = document.getElementById('nextSong')
+const backSong = document.getElementById('backSong')
+let count = 0
 
-play.addEventListener('click',() => audio.play())
+    Songs(playList, count)
 
-pause.addEventListener('click',() => audio.pause())
+    play.addEventListener('click', () => audio.play())
+    pause.addEventListener('click', () => audio.pause())
+    rewind.addEventListener('click', () => audio.currentTime -= 10)
+    forward.addEventListener('click', () => audio.currentTime += 10)
+    stops.addEventListener('click', () => {
+        audio.pause()
+        audio.currentTime = 0
+    })
+    backSong.addEventListener('click', () => {
+        count =  (count - 1) % playList.length
+        Songs(playList, count)
+    })
+    nextSong.addEventListener('click', () => {
+        count =  (count + 1) % playList.length
+        Songs(playList, count)
+    })
 
-rewind.addEventListener('click',() => audio.curruentTime -=10)
+ function Songs(song, count) {
+    playimg.src = song[count].img 
+    nameArtist.textContent = song[count].artist
+    nameSong.textContent = song[count].title
+    audioSource.src = song[count].song
 
-rewind.addEventListener('click',() => audio.curruentTime +=10)
-
-stop.addEventListener('click', () => {
-    audio.pause()
-    audio.curruentTime= 0
-})
+    audio.load()
+    audio.play()
+ }
---
+++ js/app2.js
@@ -0,0 +1,33 @@
+
+export const playList = [
+    {
+      title: 'The Resistance',
+      artist: 'Skillet',
+      img: '../assets/images/the_resistance.jpg',
+      song: "../assets/sound/Y2meta.app - Skillet - _The Resistance_ [Official Lyric Video] (128 kbps).mp3"
+    },
+    {
+      title: 'Song 2',
+      artist: 'Artist 2',
+      img: 'ruta de la imagen de la canción',
+      song: 'ruta de la cancion'
+    },
+    {
+      title: 'Song 3',
+      artist: 'Artist 3',
+      img: 'ruta de la imagen de la canción',
+      song: 'ruta de la cancion'
+    },
+    {
+      title: 'Song 4',
+      artist: 'Artist 4',
+      img: 'ruta de la imagen de la canción',
+      song: 'ruta de la cancion'
+    },
+    {
+      title: 'Song 5',
+      artist: 'Artist 5',
+      img: 'ruta de la imagen de la canción',
+      song: 'ruta de la cancion'
+    },
+  ]
